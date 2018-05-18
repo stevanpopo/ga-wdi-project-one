fcc384d0414f0c5cc3ec157d7cb5caa4b1485bcc
Stevan Popovic <stevanpopovic5@gmail.com>
2018-05-18 11:28:01 +0100

    created basic 10by10 grid in JS. Created playerCellActive feature in js and css

--- js/app.js
+++ js/app.js
@@ -1,4 +1,39 @@
+const grid = [
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
 $(() => {
   console.log('JS Log');
+
+  // Create JS grid
+  $.each(grid, (i, row) => {
+    $.each(row, (j, cell) => {
+      const $element = $('<div />');
+      //
+      if(cell === 0){
+        $element.addClass('blank');
+      } else if (cell===1){
+        // Placeholder - Adds different class if contains letter
+        $element.addClass('containsLetter');
+      }
+
+      $element.appendTo('#map');
+    });
+  });
+
+  // Highlight player active cell
+  $('#map').on('mouseover', 'div', function(){
+    this.classList.add('playerCellActive');
+  });
+
 
 });
