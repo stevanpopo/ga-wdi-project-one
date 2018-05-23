a39af243e7b905de32645ea5ef71d138b1e631a1
Stevan Popovic <stevanpopovic5@gmail.com>
2018-05-23 22:01:33 +0100

    6.6. Disable arrow keys default screen scroll so player two doesnt get jumpy screen

--- js/app.js
+++ js/app.js
@@ -211,6 +211,13 @@ $(() => {
     }
   });
 
+  window.addEventListener('keydown', function(e) {
+    // space and arrow keys
+    if([37, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+    }
+  }, false);
+
   // ###### CITIES LOGIC ######
   function randomize(array){
     const copiedArray = array.slice(); //makes a copy to randomize
