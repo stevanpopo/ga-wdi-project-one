a60cbfe6ed4808872f8bf60d8ef989dac4155041
Stevan Popovic <stevanpopovic5@gmail.com>
2018-05-25 12:23:32 +0100

    added more data

--- js/data.js
+++ js/data.js
@@ -1,15 +1,15 @@
 const capitalCitiesArray = [
   [['England', 'London', 'This city is the capital of England.'],
     ['France', 'Paris', 'This city is the capital of France.'],
-    ['Germany', 'Berlin', 'This city is the capital of Germany.']
+    ['Germany', 'Berlin', 'This city is the capital of Germany.'],
+    ['Spain', 'Madrid', 'This city is the capital of Spain.'],
+    ['Portugal', 'Lisbon', 'This city is the capital of Portugal.'],
+    ['Netherlands', 'Amsterdam', 'This city is the capital of Netherlands.'],
+    ['Ireland', 'Dublin', 'This city is the capital of Ireland.'],
+    ['Belgium', 'Brussels', 'This city is the capital of Belgium.'],
+    ['Italy', 'Rome', 'This city is the capital of Italy.'],
+    ['Serbia', 'Belgrade', 'This city is the capital of Serbia.']
     // ,
-    // ['Spain', 'Madrid', 'This city is the capital of Spain.'],
-    // ['Portugal', 'Lisbon', 'This city is the capital of Portugal.'],
-    // ['Netherlands', 'Amsterdam', 'This city is the capital of Netherlands.'],
-    // ['Ireland', 'Dublin', 'This city is the capital of Ireland.'],
-    // ['Belgium', 'Brussels', 'This city is the capital of Belgium.'],
-    // ['Italy', 'Rome', 'This city is the capital of Italy.'],
-    // ['Serbia', 'Belgrade', 'This city is the capital of Serbia.'],
     // ['Denmark', 'Copenhagen', 'This city is the capital of Denmark.'],
     // ['Sweden', 'Stockholm', 'This city is the capital of Sweden.'],
     // ['Norway', 'Oslo', 'This city is the capital of Norway.']
